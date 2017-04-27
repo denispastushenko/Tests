2a87c4a4a7cf91ff4b75603b056b5addeac52c9e
denispastushenko <dpastushenko1996@gmail.com>
2017-04-27 13:06:19 +0300

    Update MAin.java
    Main.java was edited recursive method name was changed.

--- src/main/java/MAin.java
+++ src/main/java/MAin.java
@@ -2,17 +2,16 @@ import java.util.Scanner;
 
 public class MAin {
     public static void main(String[] args) {
-        recursive();
+        recursiveMethodExample();
     }
 
-
-    private static void recursive() {
+    private static void recursiveMethodExample() {
         System.out.println("Type your digit! It should be bigger than 0 : ");
         Scanner scanner = new Scanner(System.in);
         int example = scanner.nextInt();
 
         if (example <= 0) {
-            recursive();
+           recursiveMethodExample();
         } else System.out.println("Success");
     }
 }
