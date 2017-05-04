ccf6185b2363dd568f34fd78ed20e0b3050d31c1
Denys Pastushenko <dpastushenko1996@gmail.com>
2017-05-05 02:16:13 +0300

    example 3 was modified

--- src/main/java/example3/Main.java
+++ src/main/java/example3/Main.java
@@ -1,14 +1,30 @@
 package example3;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        int firstDigit = 14;
-        int secondDigit = 3;
-        int binaryFirstDigit, binarySecondDigit;
-        binaryFirstDigit = toBinary(firstDigit);
-        binarySecondDigit = toBinary(secondDigit);
-        System.out.println( binaryFirstDigit + "    +    " + binarySecondDigit);
-        System.out.println( toBinary(firstDigit / secondDigit));
+        try {
+            int firstDigit = 14;
+            int secondDigit = 3;
+            int binaryFirstDigit, binarySecondDigit;
+            binaryFirstDigit = toBinary(firstDigit);
+            binarySecondDigit = toBinary(secondDigit);
+            System.out.println( binaryFirstDigit + "    +    " + binarySecondDigit);
+            System.out.println( toBinary(firstDigit / secondDigit));
+            System.out.println(toBinary(-128));
+            System.out.println(12 >>> 3);
+            System.out.println(-1 >>> 4);
+            System.out.println(toBinary(-1 >>> 5));
+            System.out.println(-1 >>> 5);
+            System.out.println(toBinary(-1 >> 5));
+
+
+        }catch (NumberFormatException e)
+        {
+            System.out.println(e.getMessage() + "       " + Arrays.toString(e.getStackTrace()) + "         " + e.getCause());
+
+        }
 
 
     }
