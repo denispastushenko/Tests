d408c789044cee601ae918d4fd3dfbf814d686b2
Denys Pastushenko <dpastushenko1996@gmail.com>
2017-05-07 16:52:53 +0300

    example 3 was modified

--- src/main/java/example4/Main.java
+++ src/main/java/example4/Main.java
@@ -5,8 +5,24 @@ package example4;
  */
 public class Main {
     public static void main(String[] args) {
-        if ((((true ^ false) | false) & ((~4 | 2) >= 5) && !false) | !(true & !false)) {
+        int var = 8;
+        System.out.println(++var);//9
+        System.out.println(var);//9
+        System.out.println(++var);//10
+        System.out.println(++var);//11
+        System.out.println(var);//11
+        System.out.println(++var);//12
+        System.out.println(var++);//12
+
+        if (((((true ^ false) | false) & ((~4 | 2) >= 5) && !false)) | (!(true & !false))) {//false
             System.out.println("1");
-        } else System.out.println("0");
+
+        } else if ((((++var == 5) || (false || true) && (!true)) ^ (!(!false & true))) && (var++ == 14)) {//false
+            System.out.println("11");
+
+        } else if ((true & false ^ !false && !true) & (false ^ true) || true){//true
+            System.out.println("TRUE");
+        }
+        else System.out.println("Error");
     }
 }
