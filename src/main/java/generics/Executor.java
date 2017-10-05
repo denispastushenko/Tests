c95f5aaab813beea45bc2ca517b3a086399b6db2
denys.pastushenko <denys.pastushenko@ssa-outsourcing.com>
2017-10-05 13:43:43 +0300

    generics example questions were added

--- src/main/java/generics/Executor.java
+++ src/main/java/generics/Executor.java
@@ -8,6 +8,14 @@ import java.util.List;
  */
 public class Executor {
 
+    private abstract class Example extends Product{
+        private String ex = "DDDD";
+
+        public String getEx() {
+            return ex;
+        }
+    }
+
     public static void main(String[] args) {
 
         Container<Product> example = new Container<>();
@@ -25,16 +33,37 @@ public class Executor {
         System.out.println(phoneContainer.getItem());
 
         Container container = new Container<>();
+
         List<Camera> cameras = new ArrayList<>();
         cameras.add(new Camera());
+
+        List<Phone> phones = new ArrayList<>();
+        phones.add(new Phone());
+
         List<Object> objectList = new ArrayList<>();
         objectList.add(9);
+        objectList.add(phones.stream().findFirst().get().getPixel());
         objectList.add(cameras);
         objectList.add("Str");
+
+        List<String> strings = new ArrayList<>();
+        strings.add("Yo Yo YO");
+
+        List<Example> examples = new ArrayList<>();
+
+
         container.tryPrint1(objectList);//todo ask V why
         container.tryPrint3(objectList);//todo ask V why
         //container.tryPrint1(container.tryPrint(cameras).getPrice());//todo ask V why
         container.tryPrint(cameras);
 
+        copy(phones, objectList);
+       // copy(phones,strings);//todo ask V why
+        //copy(phones, examples);//todo ask V why
+    }
+    
+    private static void copy(List<? extends Product> list1, List<? super Product> list){
+        list.addAll(list1);
+        System.out.println(list);
     }
 }
